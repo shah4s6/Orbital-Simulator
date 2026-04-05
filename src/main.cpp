9e229c750e8e280593268ebe68f73560580bcf24
shah4s6 <shah4s6@mail.uc.edu>
2026-04-05 19:04:10 -0400

    Implemented Eigen to simulate how the satellite decreases in speed as it approaches the apogee and increases in speed as it approaches to the perigee

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,26 @@
 #include "raymath.h"
 #include "../imgui/imgui.h"
 #include "../rlImGui/rlImGui.h"
+#include "eigen3/Eigen/Dense"
 
 struct Telemetry
 {
     float distance;
-    float speed;
+    float velocity;
     float trueAnomaly;
     float perigee;
     float apogee;
     float semiMajorAxis;
     float eccentricity;
 };
+
+struct Satellite
+{
+    Eigen::Vector3d position;
+    Eigen::Vector3d velocity;
+    double mass;
+};
+
 
 // Helper function to draw an elliptical orbit
 void DrawOrbitEllipse(Vector3 focus, float semiMajorAxis, float eccentricity, Color color)
@@ -23,7 +32,7 @@ void DrawOrbitEllipse(Vector3 focus, float semiMajorAxis, float eccentricity, Co
     float c = semiMajorAxis * eccentricity;
     Vector3 center = {focus.x - c, focus.y, focus.z};
     float semiMinorAxis = semiMajorAxis * sqrt(1 - eccentricity * eccentricity);
-    
+
     int segments = 200;
     for (int i = 0; i < segments; i++)
     {
@@ -58,8 +67,10 @@ float CalculateOrbitalSpeed(float distance, float semiMajorAxis)
 
 int main()
 {
-    InitWindow(800, 600, "Orbit Simulator");
+    InitWindow(1080, 720, "Orbit Simulator");
     rlImGuiSetup(true);
+
+    const double GM = 15.0;
 
     Color DARKERGRAY = GetColor(0x303234FF);
 
@@ -77,11 +88,14 @@ int main()
     // Orbit Parameters
     float semiMajorAxis = 5.0f;             // a - half the longest diameter
     float eccentricity = 0.6f;              // e - amound of stretched (0 = circle, <1 = ellipse)
-    float orbitSpeed = 1.0f;
-    float orbitAngle = 0.0f;
 
     // Initialize a satellite
-    Vector3 satellitePos = {0.0, 0.0f, 0.0f};
+    Satellite sat;
+    double r_initial = semiMajorAxis * (1 - eccentricity);
+    sat.position = Eigen::Vector3d(r_initial, 0.0, 0.0);
+    double v_perigee = sqrt(GM * (1 + eccentricity) / (semiMajorAxis * (1 - eccentricity)));
+    sat.velocity = Eigen::Vector3d(0.0, 0.0, v_perigee);
+    sat.mass = 1.0;
     float satelliteRadius = 0.5f;
 
     Telemetry telemetry = {0};
@@ -93,28 +107,47 @@ int main()
 
         if (auto_rotate)
         {
-            orbitAngle += orbitSpeed * dt;
-            if (orbitAngle > 2 * PI) orbitAngle -= 2 * PI;
-        }
+            Eigen::Vector3d r = sat.position;
+            double r_magnitude = r.norm();
 
-        float c = semiMajorAxis * eccentricity;
-        Vector3 ellipseCenter = {earthPos.x - c, earthPos.y, earthPos.z};
-        float semiMinorAxis = semiMajorAxis * sqrt(1 - eccentricity * eccentricity);
+            Eigen::Vector3d acceleration = -GM * r / (r_magnitude * r_magnitude * r_magnitude);
+
+            sat.velocity += acceleration * dt;
+            sat.position += sat.velocity * dt;
+        }
+        
+        // Update telemetry
+        Eigen::Vector3d r = sat.position;
+        Eigen::Vector3d v = sat.velocity;
+        double r_magnitude = r.norm();
+        double v_magnitude = v.norm();
+
+        telemetry.distance = r_magnitude;
+        telemetry.velocity = v_magnitude;
+
+        double epsilon = v_magnitude * v_magnitude / 2.0 - GM / r_magnitude; // epsilon - specific orbital energy 
+        telemetry.semiMajorAxis = -GM / (2.0 * epsilon);
+
+        // Eccentricity vector
+        Eigen::Vector3d e_vector = ((v_magnitude * v_magnitude - GM / r_magnitude) * r - (r.dot(v)) * v) / GM;
+        telemetry.eccentricity = e_vector.norm();
+
+        // Perigee and Apogee
+        telemetry.perigee = telemetry.semiMajorAxis * (1 - telemetry.eccentricity);
+        telemetry.apogee = telemetry.semiMajorAxis * (1 + telemetry.eccentricity);
+
+        telemetry.trueAnomaly = acos(e_vector.dot(r) / (e_vector.norm() * r_magnitude)) * 180.0 / PI;
+        if (r.dot(v) < 0) telemetry.trueAnomaly = 360.0 - telemetry.trueAnomaly;
+
+        // Calculate perigeee and apogee marker positions
+        float perigeeDistance = (float)telemetry.perigee;
+        float apogeeDistance = (float)telemetry.apogee;
+
+        Vector3 perigeePos = {earthPos.x + perigeeDistance, earthPos.y, earthPos.z};
+        Vector3 apogeePos = {earthPos.x - apogeeDistance, earthPos.y, earthPos.z};
 
         // Calculate satellite position
-        satellitePos.x = ellipseCenter.x + semiMajorAxis * cos(orbitAngle);
-        satellitePos.y = 0.0f;
-        satellitePos.z = ellipseCenter.z + semiMinorAxis * sin(orbitAngle);
-
-        // Update telemetry
-        telemetry.distance = Vector3Distance(earthPos, satellitePos);
-        telemetry.trueAnomaly = orbitAngle * 180.0f / PI;
-        telemetry.perigee = semiMajorAxis * (1 - eccentricity);
-        telemetry.apogee = semiMajorAxis * (1 + eccentricity);
-        telemetry.semiMajorAxis = semiMajorAxis;
-        telemetry.eccentricity = eccentricity;
-        telemetry.speed = CalculateOrbitalSpeed(telemetry.distance, telemetry.semiMajorAxis);
-
+        Vector3 satellitePos = {(float)sat.position.x(), (float)sat.position.y(), (float)sat.position.z()};
 
         if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON))
         {
@@ -128,33 +161,38 @@ int main()
         DrawSphere(earthPos, earthRadius, BLUE);
         DrawSphere(satellitePos, satelliteRadius, RED);
         DrawLine3D(earthPos, satellitePos, YELLOW);
-        DrawOrbitEllipse(earthPos, semiMajorAxis, eccentricity, WHITE);
+        DrawOrbitEllipse(earthPos, (float)telemetry.semiMajorAxis, (float)telemetry.eccentricity, WHITE);
+        DrawSphere(perigeePos, 0.1f, GREEN);
+        DrawSphere(apogeePos, 0.1f, YELLOW);
         DrawGrid(10, 1.0f);
         EndMode3D();
 
         rlImGuiBegin();
         ImGui::Begin("Window");
         ImGui::Text("Hold the right mouse button to rotate the view");
-        ImGui::TextColored(ImVec4 (0.0f, 1.0f, 0.0f, 1.0f), "Perigee");
-        ImGui::TextColored(ImVec4 (1.0f, 1.0f, 0.0f, 1.0f), "Apogee");
-        ImGui::SliderFloat("Semi-Major Axis (a)",&semiMajorAxis, 2.0f, 8.0f);
-        ImGui::SliderFloat("Eccentricity (e)",&eccentricity, 0.0f, 0.99f);
-        ImGui::SliderFloat("Orbit Speed",&orbitSpeed, 0.0f, 2.0f);
+
+        // ImGui::SliderFloat("Semi-Major Axis (a)",&semiMajorAxis, 2.0f, 8.0f);    TODO: Reimplement this slider correctly
+        // ImGui::SliderFloat("Eccentricity (e)",&eccentricity, 0.0f, 0.99f);       TODO: Reimplement this slider correctly
+        // ImGui::SliderFloat("Speed", &telemetry.velocity, 0.0f, 5.0f);            TODO: Reimplement this slider correctly
         ImGui::Checkbox("Automatic Rotation", &auto_rotate);
         ImGui::Separator();
         ImGui::Text("Distance: %.2f", telemetry.distance);
-        ImGui::Text("Speed: %.2f", telemetry.speed);
         ImGui::Text("True Anomaly: %.1f", telemetry.trueAnomaly);
-        ImGui::Text("Perigee: %.2f | Apogee: %.2f", telemetry.perigee, telemetry.apogee);
+        ImGui::TextColored(ImVec4 (0.0f, 1.0f, 0.0f, 1.0f), "Perigee: %.2f", telemetry.perigee);
+        ImGui::TextColored(ImVec4 (1.0f, 1.0f, 0.0f, 1.0f), "Apogee: %.2f", telemetry.apogee);
         ImGui::Separator();
 
         if (ImGui::Button("Reset"))
         {
             semiMajorAxis = 5.0f;
             eccentricity = 0.6f;
-            orbitSpeed = 1.0f;
-            orbitAngle = 0.0f;
             auto_rotate = true;
+            
+            // Reset satellite state
+            double r_initial = semiMajorAxis * (1 - eccentricity);
+            sat.position = Eigen::Vector3d(r_initial, 0.0, 0.0);
+            double v_perigee = sqrt(GM * (1 + eccentricity) / (semiMajorAxis * (1 - eccentricity)));
+            sat.velocity = Eigen::Vector3d(0.0, 0.0, v_perigee);
         }
 
         ImGui::End();
